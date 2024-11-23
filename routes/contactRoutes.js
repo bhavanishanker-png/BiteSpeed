be07018bdb19fa3b5301a01f8e6c3193498edbb5
bhavani shanker <bhavanishanker@bhavanis-MacBook-Pro.local>
2024-11-23 18:08:36 +0530

    hello

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,5 +4,7 @@ const { identifyContact } = require('../controllers/contactController');
 const router = express.Router();
 
 router.post('/identify', identifyContact);
-
+router.get("/", (req, res) => {
+    res.send("Hello, world!");
+});
 module.exports = router;
